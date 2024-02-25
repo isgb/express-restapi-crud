4e9fe8c224abd7bade4caeb5cbec87570581afb7
isaias_pruebas <isgb_codigo@hotmail.com>
2024-02-24 23:02:34 -0600

    debug

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const morgan = require('morgan')
+
+const app = express();
+
+app.use(morgan('dev'));
+
+app.get('/products', (req,res) => {
+    res.send('obteniendo productos')
+})
+
+app.post('/products', (req,res) => {
+    res.send('creando productos')
+})
+
+app.put('/products', (req,res) => {
+    res.send('actualizando productos')
+})
+
+app.delete('/products', (req,res) => {
+    res.send('borrando productos')
+})
+
+app.get('/products/:id', (req,res) => {
+    res.send('obteniendo un productos')
+})
+
+app.listen(3000);
+console.log(`SERVER ON PORT ${3000}`)
